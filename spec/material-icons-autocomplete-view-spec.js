8731d8af2c54b9cb90d28cf5dbec3c15ba365ead
kimjs3550 <35187730+kimjs3550@users.noreply.github.com>
2018-05-06 14:17:21 +0900

    Initial commit

---
+++ spec/material-icons-autocomplete-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MaterialIconsAutocompleteView from '../lib/material-icons-autocomplete-view';
+
+describe('MaterialIconsAutocompleteView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
